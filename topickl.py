ce39b67ecbb2b766cb439ef0c67e1b48c250aa87
juntkd <junaid.cet@gmail.com>
2022-01-19 20:45:59 +0400

    File deleted

--- topickl.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import joblib
-reviews_data = pd.read_csv('DataFiles/Sample30.csv')
-reviews_data = reviews_data[['name', 'reviews_rating', 'reviews_text', 'user_sentiment']]
-joblib.dump(reviews_data, 'Pickles/reviews_data.pkl')
